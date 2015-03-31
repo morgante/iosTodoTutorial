3079515978a3aea9263f32c1c9a98a9aa02f3219
Morgante Pell <morgante.pell@morgante.net>
2015-03-30 23:50:27 -0400

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Morgante Pell on 3/30/15.
+//  Copyright (c) 2015 Morgante Pell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
