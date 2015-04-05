9bebced10d44038a4a572654c4b8f8c5187126b4
Morgante Pell <morgante.pell@morgante.net>
2015-04-05 04:06:54 -0400

    allow adding of items from the new item view

--- ToDoList/ToDoListTableViewController.m
+++ ToDoList/ToDoListTableViewController.m
@@ -8,6 +8,7 @@
 
 #import "ToDoListTableViewController.h"
 #import "ToDoItem.h"
+#import "AddToDoItemViewController.h"
 
 @interface ToDoListTableViewController ()
 
@@ -30,7 +31,13 @@
 }
 
 - (IBAction)unwindToList:(UIStoryboardSegue *)segue {
+    AddToDoItemViewController *source = [segue sourceViewController];
+    ToDoItem *item = source.toDoItem;
     
+    if (item != nil) {
+        [self.toDoItems addObject:item];
+        [self.tableView reloadData];
+    }
 }
 
 - (void)viewDidLoad {
